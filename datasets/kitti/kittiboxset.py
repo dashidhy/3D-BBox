b41b3ecd34a31f87b7a42657224da707b3a6f17e
Hongyuan Du <du-hongyuan@outlook.com>
2020-04-24 21:05:33 +0000

    finish KittiBoxSet class

--- datasets/kitti/kittiboxset.py
+++ datasets/kitti/kittiboxset.py
@@ -1,5 +1,6 @@
 import os
 from PIL import Image
+import torch
 from torch.utils.data import Dataset
 if __name__ == '__main__':
     import kitti_utils as ku
@@ -7,26 +8,62 @@ else:
     from . import kitti_utils as ku
 
 __all__ = [
-    'KittiBoxSet'
+    'box_label2tensor', 'KittiBoxSet'
 ]
+
+
+def box_label2tensor(box_label):
+    for key, val in box_label.items():
+        if not isinstance(val, str):
+            box_label[key] = torch.tensor(val)
+    return box_label
+
 
 class KittiBoxSet(Dataset):
 
-    def __init__(self, kittiroot, split, transform=None, target_transform=None):
+    def __init__(self, kitti_root, split, transform=None, target_transform=None):
         assert split in ['train', 'val']
         super(KittiBoxSet, self).__init__()
         self.split = split
-        self.image_dir = os.path.join(kittiroot, 'boxes', split, 'image')
-        self.label_dir = os.path.join(kittiroot, 'boxes', split, 'label')
+        self.image_dir = os.path.join(kitti_root, 'boxes', split, 'image')
+        self.label_dir = os.path.join(kitti_root, 'boxes', split, 'label')
 
-        self.box_ids = [filename[:-4] for filename in os.listdir(self.image_dir)]
-        self.num_boxes = len(self.box_ids)
+        self.image_files = os.listdir(self.image_dir)
 
         self.transform = transform
         self.target_transform = target_transform
     
     def __len__(self):
-        return self.num_boxes
+        return len(self.image_files)
     
     def __getitem__(self, idx):
-        return self.box_ids[idx]
+        # get image
+        box_image = Image.open(os.path.join(self.image_dir, self.image_files[idx]))
+        if self.transform is not None:
+            box_image = self.transform(box_image)
+        
+        # get label
+        box_label = ku.read_box_label(os.path.join(self.label_dir, self.image_files[idx][:-4]+'.txt'))
+        if self.target_transform is not None:
+            box_label = self.target_transform(box_label)
+        
+        return box_image, box_label
+
+
+# debug
+if __name__ == '__main__':
+    from torch.utils.data import DataLoader
+    from torchvision import transforms as T
+
+    kitti_root = '/home/srip19-pointcloud/datasets/KITTI'
+    trans = T.ToTensor()
+
+    boxset = KittiBoxSet(kitti_root, 'train', transform=trans, target_transform=box_label2tensor)
+    loader = DataLoader(boxset, batch_size=4, shuffle=True)
+
+    for batch in loader:
+        batch_box_tensor, batch_box_label = batch
+        break
+
+    print(batch_box_tensor.size())
+    print(batch_box_label)
