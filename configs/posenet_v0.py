bbe0c07c98724e2b1e6d2936341d69af750b1cff
Hongyuan Du <du-hongyuan@outlook.com>
2020-04-27 12:18:15 +0000

    snapshot of config and train

--- configs/posenet_v0.py
+++ configs/posenet_v0.py
@@ -1,31 +1,46 @@
 # global
-NUM_BINS = 4
+__NUM_BINS = 4
+
+# dataset settings
+dataset_cfg = dict(
+    kitti_root = './data/kitti',
+    img_norm = {'mean': (0.485, 0.456, 0.406), 'std': (0.229, 0.224, 0.225)},
+    del_labels = ('sample', 'type', 'class')
+)
 
 # model settings
-model = dict(
+model_cfg = dict(
     type = 'PoseNet',
-    backbone_cfg = dict(type = 'resnet34', 
-                        pretrained = True),
+    backbone_cfg = dict(type = 'resnet34',
+                        pretrained = True,
+                        progress = False),
     head_cfg = dict(type = 'BoxHead',
                     in_size = 512 * 7 * 7,
-                    num_bins = NUM_BINS,
-                    d_hidden_sizes = [512],
-                    a_hidden_sizes = [256],
-                    c_hidden_sizes = [256])
+                    num_bins = __NUM_BINS,
+                    dim_reg_hide_sizes = [512],
+                    bin_conf_hide_sizes = [256],
+                    bin_reg_hide_sizes = [256])
 )
 
 # loss settings
-dimension_loss = dict(
-    type = 'Dimension_Loss',
-    base_loss_cfg = dict(type = 'MSE'),
-    avg_dim = [1.61057209, 1.47745965, 3.52359498] # (h, w, l)
+loss_cfg = dict(
+    dimension_loss_cfg = dict(type = 'Dimension_Loss',
+                              base_loss_cfg = dict(type = 'MSE'),
+                              avg_dim = [1.61057209, 1.47745965, 3.52359498]),
+    pose_loss_cfg = dict(type = 'Pose_Loss',
+                         base_conf_cfg = dict(type = 'CrossEntropy'),
+                         base_reg_cfg = dict(type = 'Cosine_Expansion', 
+                                             normalize = True),
+                         num_bins = __NUM_BINS,
+                         bin_range_degree = 120),
+    loss_weights = {'dim_reg': 1.0, 'bin_conf': 1.0, 'bin_reg': 1.0}
 )
 
-pose_loss = dict(
-    type = 'Pose_Loss',
-    base_conf_cfg = dict(type = 'CrossEntropy'),
-    base_reg_cfg = dict(type = 'Cosine_Expansion', 
-                        normalize = True),
-    num_bins = NUM_BINS,
-    bin_range_degree = 120
+# training settings
+training_cfg = dict(
+    loader_cfg = dict(batch_size = 8, 
+                      num_workers = 4,
+                      pin_memory = True,
+                      drop_last = True)
+
 )
--- train.py
+++ train.py
@@ -1,23 +1,38 @@
+import os
 import argparse
-import torch
+from torch.utils.data import DataLoader
 from miscs import config_utils as cu
-from models import PoseNet
+from datasets.kitti import KittiBoxSet
+import models
+from models.builder import build_from, build_loss
+from datasets.kitti.kitti_utils import box_label2tensor, box_image2input
 
 parser = argparse.ArgumentParser()
-parser.add_argument('--cfg_file', type=str, help='Config file path.')
+parser.add_argument('-c', '--cfg_file', type=str, required=True, help='Config file path, required flag.')
+parser.add_argument('--kitti_root', type=str, help='KITTI dataset root.')
+parser.add_argument('--batch_size', type=int, help='Mini-batch size')
+parser.add_argument('--num_workers', type=int, help='Number of workers for DataLoader')
 FLAGS = parser.parse_args()
 
+# parse configs
 cfg_dict = cu.file2dict(FLAGS.cfg_file)
+dataset_cfg = cu.parse_args_update(FLAGS, cfg_dict['dataset_cfg'])
+model_cfg = cfg_dict['model_cfg']
+loss_cfg = cfg_dict['loss_cfg']
+training_cfg = cfg_dict['training_cfg']
+loader_cfg = cu.parse_args_update(FLAGS, training_cfg['loader_cfg'])
+
+# build dataset and dataloader
+train_set = KittiBoxSet(kitti_root=dataset_cfg['kitti_root'], split='train', 
+                        transform=box_image2input(**dataset_cfg['img_norm']), 
+                        label_transform=box_label2tensor(dataset_cfg['del_labels']))
+
+train_loader = DataLoader(train_set, shuffle=True, **loader_cfg)
+
 
 # build model
-model_cfg = cfg_dict['model']
-posenet = PoseNet(backbone_cfg=model_cfg['backbone_cfg'],
-                  head_cfg=model_cfg['head_cfg'])
+posenet = build_from(models, model_cfg)
 
-
-# debug
-fake_img_batch = torch.randn([8, 3, 224, 224])
-out = posenet(fake_img_batch)
-
-for ele in out:
-    print(ele.size())
+# build loss
+dimension_loss = build_loss(loss_cfg['dimension_loss_cfg'])
+pose_loss = build_loss(loss_cfg['pose_loss_cfg'])
