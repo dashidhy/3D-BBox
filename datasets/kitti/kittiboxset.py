0813ad17666fe9268f8e89b5d1fd6316ec66ab9f
Hongyuan Du <du-hongyuan@outlook.com>
2020-04-27 08:08:56 +0000

    change target_transform to label_transform to differentiate target and label

--- datasets/kitti/kittiboxset.py
+++ datasets/kitti/kittiboxset.py
@@ -11,7 +11,7 @@ __all__ = [
 
 class KittiBoxSet(Dataset):
 
-    def __init__(self, kitti_root, split, transform=None, target_transform=None):
+    def __init__(self, kitti_root, split, transform=None, label_transform=None):
         assert split in ['train', 'val']
         super(KittiBoxSet, self).__init__()
         self.split = split
@@ -21,7 +21,7 @@ class KittiBoxSet(Dataset):
         self.image_files = os.listdir(self.image_dir)
 
         self.transform = transform
-        self.target_transform = target_transform
+        self.label_transform = label_transform
     
     def __len__(self):
         return len(self.image_files)
@@ -34,7 +34,7 @@ class KittiBoxSet(Dataset):
         
         # get label
         box_label = ku.read_box_label(os.path.join(self.label_dir, self.image_files[idx][:-4]+'.txt'))
-        if self.target_transform is not None:
-            box_label = self.target_transform(box_label)
+        if self.label_transform is not None:
+            box_label = self.label_transform(box_label)
         
         return box_image, box_label
