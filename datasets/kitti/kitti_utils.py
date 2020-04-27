7a57fb5b20eaebfa16789f9d96b85b3aaec1a68e
Hongyuan Du <du-hongyuan@outlook.com>
2020-04-27 08:12:19 +0000

    add input preprocessing method

--- datasets/kitti/kitti_utils.py
+++ datasets/kitti/kitti_utils.py
@@ -2,10 +2,11 @@ import os
 from PIL import Image
 import numpy as np
 import torch
+from torchvision import transforms as T
 
 __all__ = [
     'read_txt_lines', 'read_scene_labels', 'read_box_label', 'label_scene2box',
-    'write_box_label', 'write_box_image', 'box_label2tensor'
+    'write_box_label', 'write_box_image', 'box_label2tensor', 'box_image2input'
 ]
 
 
@@ -97,8 +98,19 @@ def write_box_image(box_image, filepath, force=False):
     return
 
 
-def box_label2tensor(box_label):
-    for key, val in box_label.items():
-        if not isinstance(val, str):
-            box_label[key] = torch.tensor(val)
-    return box_label
+class box_label2tensor(object):
+
+    def __call__(self, box_label):
+        for key, val in box_label.items():
+            if not isinstance(val, str):
+                box_label[key] = torch.tensor(val)
+        return box_label
+
+
+class box_image2input(object):
+
+    def __init__(self, mean=(0.485, 0.456, 0.406), std=(0.229, 0.224, 0.225)):
+        self.transform = T.Compose([T.ToTensor(), T.Normalize(mean=mean, std=std)])
+    
+    def __call__(self, box_image):
+        return self.transform(box_image)
