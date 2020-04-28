a105e0122bace0e054b1ca286eb1612122c530d0
Hongyuan Du <du-hongyuan@outlook.com>
2020-04-28 02:58:24 +0000

    ready for writting training code

--- configs/posenet_v0.py
+++ configs/posenet_v0.py
@@ -10,10 +10,13 @@ dataset_cfg = dict(
 
 # model settings
 model_cfg = dict(
+    
     type = 'PoseNet',
+    
     backbone_cfg = dict(type = 'resnet34',
                         pretrained = True,
                         progress = False),
+    
     head_cfg = dict(type = 'BoxHead',
                     in_size = 512 * 7 * 7,
                     num_bins = __NUM_BINS,
@@ -24,23 +27,40 @@ model_cfg = dict(
 
 # loss settings
 loss_cfg = dict(
+    
     dimension_loss_cfg = dict(type = 'Dimension_Loss',
                               base_loss_cfg = dict(type = 'MSE'),
                               avg_dim = [1.61057209, 1.47745965, 3.52359498]),
+    
     pose_loss_cfg = dict(type = 'Pose_Loss',
                          base_conf_cfg = dict(type = 'CrossEntropy'),
                          base_reg_cfg = dict(type = 'Cosine_Expansion', 
                                              normalize = True),
                          num_bins = __NUM_BINS,
                          bin_range_degree = 120),
+    
     loss_weights = {'dim_reg': 1.0, 'bin_conf': 1.0, 'bin_reg': 1.0}
 )
 
 # training settings
 training_cfg = dict(
+    
     loader_cfg = dict(batch_size = 8, 
                       num_workers = 4,
                       pin_memory = True,
-                      drop_last = True)
+                      drop_last = True),
+    
+    optimizer_cfg = dict(type ='SGD',
+                         lr = 1e-4, 
+                         momentum = 0.9, 
+                         dampening = 0, 
+                         weight_decay = 1e-4, 
+                         nesterov = False),
+    
+    total_epoch = 10
+    
+)
 
+log_cfg = dict(
+    log_dir = './run'
 )
--- train.py
+++ train.py
@@ -1,7 +1,8 @@
 import os
 import argparse
+import torch
 from torch.utils.data import DataLoader
-from miscs import config_utils as cu
+from miscs import config_utils as cu, X_Logger
 from datasets.kitti import KittiBoxSet
 import models
 from models.builder import build_from, build_loss
@@ -9,6 +10,7 @@ from datasets.kitti.kitti_utils import box_label2tensor, box_image2input
 
 parser = argparse.ArgumentParser()
 parser.add_argument('-c', '--cfg_file', type=str, required=True, help='Config file path, required flag.')
+parser.add_argument('--log_dir', type=str, help='Folder to save experiment records.')
 parser.add_argument('--kitti_root', type=str, help='KITTI dataset root.')
 parser.add_argument('--batch_size', type=int, help='Mini-batch size')
 parser.add_argument('--num_workers', type=int, help='Number of workers for DataLoader')
@@ -16,11 +18,17 @@ FLAGS = parser.parse_args()
 
 # parse configs
 cfg_dict = cu.file2dict(FLAGS.cfg_file)
-dataset_cfg = cu.parse_args_update(FLAGS, cfg_dict['dataset_cfg'])
-model_cfg = cfg_dict['model_cfg']
-loss_cfg = cfg_dict['loss_cfg']
-training_cfg = cfg_dict['training_cfg']
-loader_cfg = cu.parse_args_update(FLAGS, training_cfg['loader_cfg'])
+dataset_cfg = cu.parse_args_update(FLAGS, cfg_dict['dataset_cfg']).copy()
+model_cfg = cfg_dict['model_cfg'].copy()
+loss_cfg = cfg_dict['loss_cfg'].copy()
+training_cfg = cfg_dict['training_cfg'].copy()
+loader_cfg = cu.parse_args_update(FLAGS, training_cfg['loader_cfg']).copy()
+optimizer_cfg = cu.parse_args_update(FLAGS, training_cfg['optimizer_cfg']).copy()
+log_cfg = cu.parse_args_update(FLAGS, cfg_dict['log_cfg']).copy()
+
+# log parse_args
+logger = X_Logger(log_cfg['log_dir'])
+logger.add_parse_args(FLAGS)
 
 # build dataset and dataloader
 train_set = KittiBoxSet(kitti_root=dataset_cfg['kitti_root'], split='train', 
@@ -31,8 +39,12 @@ train_loader = DataLoader(train_set, shuffle=True, **loader_cfg)
 
 
 # build model
-posenet = build_from(models, model_cfg)
+posenet = build_from(models, model_cfg).cuda()
 
 # build loss
-dimension_loss = build_loss(loss_cfg['dimension_loss_cfg'])
-pose_loss = build_loss(loss_cfg['pose_loss_cfg'])
+dimension_loss = build_loss(loss_cfg['dimension_loss_cfg'].copy()).cuda()
+pose_loss = build_loss(loss_cfg['pose_loss_cfg'].copy()).cuda()
+
+# build optimizer
+optim_type = getattr(torch.optim, optimizer_cfg.pop('type'))
+optimizer = optim_type(posenet.parameters(), **optimizer_cfg)
