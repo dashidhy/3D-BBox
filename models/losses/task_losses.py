aa3cbe69f17fcd0450cddf8a26c97dfeb462c050
Hongyuan Du <du-hongyuan@outlook.com>
2020-04-28 01:42:00 +0000

    rewrite task losses by nn.Module

--- models/losses/task_losses.py
+++ models/losses/task_losses.py
@@ -1,4 +1,5 @@
 import torch
+from torch import nn
 import numpy as np
 from . import base_losses
 
@@ -12,23 +13,23 @@ def build_base_loss(cfg):
     return attr(**cfg)
 
 
-class Dimension_Loss(object):
+class Dimension_Loss(nn.Module):
 
     def __init__(self, base_loss_cfg, avg_dim=[1., 1., 1.]):
         self.base_loss = build_base_loss(base_loss_cfg)
-        self.avg_dim = torch.tensor(avg_dim).float()
+        self.register_buffer('avg_dim', torch.tensor(avg_dim).float())
     
-    def __call__(self, value, label, weight=None, reduction='mean'):
+    def forward(self, value, label, weight=None, reduction='mean'):
         target = label - self.avg_dim.to(label.device)
         return self.base_loss(value, target, weight, reduction)
 
 
-class Pose_Loss(object):
+class Pose_Loss(nn.Module):
 
     def __init__(self, base_conf_cfg, base_reg_cfg, num_bins, bin_range_degree):
         self.base_conf_loss = build_base_loss(base_conf_cfg)
         self.base_reg_loss = build_base_loss(base_reg_cfg)
-        self.bin_centers = torch.arange(num_bins).float() * 2 * np.pi / num_bins
+        self.register_buffer('bin_centers', torch.arange(num_bins).float() * 2 * np.pi / num_bins)
         self.bin_cos_half_range = np.cos(bin_range_degree * np.pi / 360.0)
     
     def label2targets(self, label):
@@ -38,10 +39,9 @@ class Pose_Loss(object):
         conf_target = torch.argmax(reg_target_cos, dim=1)
         return conf_target, reg_target, reg_mask
     
-    def __call__(self, conf_value, reg_value, label, 
-                 conf_weight=None, reg_weight=None,
-                 conf_reduction='mean',
-                 reg_reduction='mean'):
+    def forward(self, conf_value, reg_value, label, 
+                conf_weight=None, reg_weight=None,
+                conf_reduction='mean', reg_reduction='mean'):
         conf_target, reg_target, reg_mask = self.label2targets(label)
         reg_final_weight = reg_mask
         if reg_weight is not None: 
