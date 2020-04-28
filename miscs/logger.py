6861ab4fb4ee79ff92eaf02c73b1310a287c8681
Hongyuan Du <du-hongyuan@outlook.com>
2020-04-28 06:04:59 +0000

    finish a basic logger

--- miscs/logger.py
+++ miscs/logger.py
@@ -1,10 +1,27 @@
 import os
+import logging
 import torch
 from torch.utils.tensorboard import SummaryWriter
 
 __all__ = [
     'X_Logger'
 ]
+
+
+def create_logger(log_file, rank=0, log_level=logging.INFO):
+    logger = logging.getLogger(__name__)
+    logger.setLevel(log_level if rank == 0 else 'ERROR')
+    formatter = logging.Formatter('%(asctime)s  %(levelname)5s  %(message)s')
+    console = logging.StreamHandler()
+    console.setLevel(log_level if rank == 0 else 'ERROR')
+    console.setFormatter(formatter)
+    file_handler = logging.FileHandler(filename=log_file)
+    file_handler.setLevel(log_level if rank == 0 else 'ERROR')
+    file_handler.setFormatter(formatter)
+    logger.addHandler(console)
+    logger.addHandler(file_handler)
+    return logger
+
 
 class X_Logger(SummaryWriter):
 
@@ -16,6 +33,7 @@ class X_Logger(SummaryWriter):
                                        max_queue, flush_secs, filename_suffix)
         self.ckpt_dir = os.path.join(self.root_dir, 'checkpoints')
         os.mkdir(self.ckpt_dir)
+        self.cmd_logger = create_logger(os.path.join(self.root_dir, 'log_train.txt'))
     
     def add_parse_args(self, parse_args):
         with open(os.path.join(self.root_dir, 'parse_args.txt'), 'w') as f:
@@ -28,3 +46,6 @@ class X_Logger(SummaryWriter):
         if ckpt_name is None:
             ckpt_name = 'ckpt_%04d.tar'% num_epoch
         torch.save(save_dict, os.path.join(self.ckpt_dir, ckpt_name))
+    
+    def info(self, string):
+        self.cmd_logger.info(string)
