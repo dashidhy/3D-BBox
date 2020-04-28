183007ab305f4abfe877233a19e99b9a25768f2b
Hongyuan Du <du-hongyuan@outlook.com>
2020-04-28 06:03:54 +0000

    fix division bug

--- models/losses/base_losses.py
+++ models/losses/base_losses.py
@@ -75,6 +75,6 @@ class Cosine_Expansion(BaseLoss):
 
         if self.normalize:
             value_norm = torch.norm(value, dim=-1, keepdim=True)
-            value /= value_norm
+            value = torch.div(value, value_norm)
 
         return -(torch.cos(target) * value[..., 0] + torch.sin(target) * value[..., 1])
