282e3de119bc5f49399734225439fee3eeade6b3
Hongyuan Du <du-hongyuan@outlook.com>
2020-05-12 01:27:37 +0000

    rename for module import

--- bbox3D_estimate.py
+++ bbox3D_estimate.py
@@ -44,74 +44,120 @@ def solve_3d_bbox_single(bbox2D, corners, theta_l, calib):
 
     corners = torch.matmul(corners, Ry_T) # rotated corners
 
-    # prepare constrains
+    # adjust front side
+    if theta_l >= np.pi / 2.0 and theta_l < np.pi:
+        corners = corners[[3, 0, 1, 2, 7, 4, 5, 6]].contiguous()
+    elif theta_l >= -np.pi and theta_l < -np.pi / 2.0:
+        corners = corners[[2, 3, 0, 1, 6, 7, 4, 5]].contiguous()
+    elif theta_l >= -np.pi / 2.0 and theta_l < 0.0:
+        corners = corners[[1, 2, 3, 0, 5, 6, 7, 4]].contiguous()
+
+    # start solve constrains
     X = torch.eye(4)
+    A = torch.zeros(4, 3)
+    b = torch.zeros(4)
+
+    # prepare constrains
     constrains = {}
 
-    # x1, x2, y1 -> 4, 5, 6, 7
+    # x1 -> 7, 6
     constrains['x1'] = {}
-    constrains['x2'] = {}
-    constrains['y1'] = {}
-    constrains['y2'] = {}
 
-    for i in [4, 5, 6, 7]:
+    for i in [7, 6]:
+        constrains['x1'][i] = {}
         X[:3, 3] = corners[i]
         K_X = torch.matmul(K, X)
-
-        constrains['x1'][i] = {}
         constrains['x1'][i]['A'] = K_X[0, :3] - x1 * K_X[2, :3]
         constrains['x1'][i]['b'] = x1 * K_X[2, 3] - K_X[0, 3]
+    
+    # x2 -> 4, 7
+    constrains['x2'] = {}
 
+    for i in [4, 7]:
         constrains['x2'][i] = {}
-        constrains['x2'][i]['A'] = K_X[0, :3] - x2 * K_X[2, :3]
-        constrains['x2'][i]['b'] = x2 * K_X[2, 3] - K_X[0, 3]
-
-        constrains['y1'][i] = {}
-        constrains['y1'][i]['A'] = K_X[1, :3] - y1 * K_X[2, :3]
-        constrains['y1'][i]['b'] = y1 * K_X[2, 3] - K_X[1, 3]
-
-    # y2 -> 0, 1, 2, 3
-    for i in [0, 1, 2, 3]:
         X[:3, 3] = corners[i]
         K_X = torch.matmul(K, X)
+        constrains['x2'][i]['A'] = K_X[0, :3] - x2 * K_X[2, :3]
+        constrains['x2'][i]['b'] = x2 * K_X[2, 3] - K_X[0, 3]
+    
+    # y1 -> 4, 5, 6, 7
+    constrains['y1'] = {}
 
+    for i in [4, 5, 6, 7]:
+        constrains['y1'][i] = {}
+        X[:3, 3] = corners[i]
+        K_X = torch.matmul(K, X)
+        constrains['y1'][i]['A'] = K_X[1, :3] - y1 * K_X[2, :3]
+        constrains['y1'][i]['b'] = y1 * K_X[2, 3] - K_X[1, 3]
+    
+    # y2 -> 2, 3, 0
+    constrains['y2'] = {}
+
+    for i in [2, 3, 0]:
         constrains['y2'][i] = {}
+        X[:3, 3] = corners[i]
+        K_X = torch.matmul(K, X)
         constrains['y2'][i]['A'] = K_X[1, :3] - y2 * K_X[2, :3]
         constrains['y2'][i]['b'] = y2 * K_X[2, 3] - K_X[1, 3]
 
     # solving linear functions
-    A = torch.zeros(4, 3)
-    b = torch.zeros(4)
     error = float('inf')
 
-    crp = [{'x1': [6, 7], 'x2': [4], 'y1':[5, 7], 'y2': [3]},
-           {'x1': [5, 6], 'x2': [7], 'y1':[4, 6], 'y2': [2]},
-           {'x1': [4, 5], 'x2': [6], 'y1':[7, 5], 'y2': [1]},
-           {'x1': [7, 4], 'x2': [5], 'y1':[6, 4], 'y2': [0]}]
+    # case 1: only see front side
+    A[0] = constrains['x1'][7]['A']
+    b[0] = constrains['x1'][7]['b']
+    A[1] = constrains['x2'][4]['A']
+    b[1] = constrains['x2'][4]['b']
 
-    for i in range(4):
-        cr = crp[i]
+    for i in [3, 0]:
+        A[2] = constrains['y2'][i]['A']
+        b[2] = constrains['y2'][i]['b']
+        for j in [4, 5, 6, 7]:
+            A[3] = constrains['y1'][j]['A']
+            b[3] = constrains['y1'][j]['b']
 
-        for x_1 in cr['x1']:
-            A[0] = constrains['x1'][x_1]['A']
-            b[0] = constrains['x1'][x_1]['b']
-            for x_2 in cr['x2']:
-                A[1] = constrains['x2'][x_2]['A']
-                b[1] = constrains['x2'][x_2]['b']
-                for y_1 in cr['y1']:
-                    A[2] = constrains['y1'][y_1]['A']
-                    b[2] = constrains['y1'][y_1]['b']
-                    for y_2 in cr['y2']:
-                        A[3] = constrains['y2'][y_2]['A']
-                        b[3] = constrains['y2'][y_2]['b']
+            trans_t = torch.matmul(torch.pinverse(A), b)
+            error_t = torch.norm(torch.matmul(A, trans_t) - b)
 
-                        trans_t = torch.matmul(torch.pinverse(A), b)
-                        error_t = torch.norm(torch.matmul(A, trans_t) - b)
+            if error_t < error:
+                trans = trans_t
+                error = error_t
+    
+    # case 2: see both front side and lateral side
+    A[0] = constrains['x1'][6]['A']
+    b[0] = constrains['x1'][6]['b']
 
-                        if error_t < error:
-                            trans = trans_t
-                            error = error_t
+    for i in [2, 3, 0]:
+        A[2] = constrains['y2'][i]['A']
+        b[2] = constrains['y2'][i]['b']
+        for j in [4, 5, 6, 7]:
+            A[3] = constrains['y1'][j]['A']
+            b[3] = constrains['y1'][j]['b']
 
+            trans_t = torch.matmul(torch.pinverse(A), b)
+            error_t = torch.norm(torch.matmul(A, trans_t) - b)
+
+            if error_t < error:
+                trans = trans_t
+                error = error_t
+    
+    # case 2: only see lateral side
+    A[1] = constrains['x2'][7]['A']
+    b[1] = constrains['x2'][7]['b']
+
+    for i in [2, 3]:
+        A[2] = constrains['y2'][i]['A']
+        b[2] = constrains['y2'][i]['b']
+        for j in [4, 5, 6, 7]:
+            A[3] = constrains['y1'][j]['A']
+            b[3] = constrains['y1'][j]['b']
+
+            trans_t = torch.matmul(torch.pinverse(A), b)
+            error_t = torch.norm(torch.matmul(A, trans_t) - b)
+
+            if error_t < error:
+                trans = trans_t
+                error = error_t
     
     return trans
 
@@ -128,8 +174,6 @@ if __name__ == '__main__':
     box_label = ku.read_box_label(os.path.join(box_root, 'label', box_id+'.txt'))
     scene_id = box_label['sample']
     calib = ku.read_calib(os.path.join(scene_root, 'calib', scene_id+'.txt'))
-
-    scene_label = ku.read_scene_labels(os.path.join(scene_root, 'label_2', scene_id+'.txt'))
 
     bbox2D = torch.tensor(box_label['bbox2D'])
     corners = dimensions_to_corners(torch.tensor(box_label['dimensions']).unsqueeze(0)).squeeze()
